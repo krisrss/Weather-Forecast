904d7e10058de0872cd9a7f886b7afb32062ea3e
Kris <kristapsrss@gmail.com>
2018-02-21 15:51:02 +0200

    Add style to current weather data section

--- app/services/getWeatherData.js
+++ app/services/getWeatherData.js
@@ -6,7 +6,7 @@
 
         //NOTE : for testing purposes, displays only data for London
         var currentWeather = function() {
-            return $http.get("http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=4a3e30b2283e445fc4c685a6a02916cd")
+            return $http.get("http://api.openweathermap.org/data/2.5/weather?q=london&units=metric&appid=4a3e30b2283e445fc4c685a6a02916cd")
                 .then(function(response){
                     return response.data;
                 })
