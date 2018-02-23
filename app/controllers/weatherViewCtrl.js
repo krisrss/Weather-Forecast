6468026f338d606c6cd705322974d8c214466a67
Kris <kristapsrss@gmail.com>
2018-02-23 18:40:30 +0200

    Display day times with corresponding alias

--- app/controllers/weatherViewCtrl.js
+++ app/controllers/weatherViewCtrl.js
@@ -25,14 +25,38 @@
         );
 
 
-
+        vm.extractTime = function(data){
+            var forecastDate = data + '';
+            var splitTime = forecastDate.split(" ");
+            return splitTime[1];
+        };
 
         vm.filterData = function (data) {
-            var forecastDate = data.dt_txt + '';
-            var splitTime = forecastDate.split(" ");
-            var forecastTime = splitTime[1].slice(0, -3);
-            return (forecastTime === '00:00' || forecastTime === '06:00' || forecastTime === '12:00' || forecastTime === '18:00' );
-        }
+           var forecastTime = vm.extractTime(data.dt_txt);
+           return (forecastTime === '00:00:00' || forecastTime === '06:00:00' || forecastTime === '12:00:00' || forecastTime === '18:00:00' );
+        };
+
+        //Set alias for day period
+        vm.setDayPrefix = function(time){
+            switch(time) {
+                case "00:00:00":
+                    return "Night";
+                    break;
+                case "06:00:00":
+                    return "Morning";
+                    break;
+                case "12:00:00":
+                    return "Day";
+                    break;
+                case "18:00:00":
+                    return "Evening";
+                    break;
+                default:
+                return "Wrong Time"
+            };
+        };
+
+
     };
 
 }());
