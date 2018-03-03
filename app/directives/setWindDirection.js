daf357880e2dd274bec325d5b2eccc33501ae3c9
Kris <kristapsrss@gmail.com>
2018-03-03 15:26:31 +0200

    Fix wind arrow, for multiple instances

--- app/directives/setWindDirection.js
+++ app/directives/setWindDirection.js
@@ -1,4 +1,4 @@
-(function(){
+(function () {
     "use strict";
 
     angular.module("WeatherApp").directive('setWindDir', setWindDir);
@@ -7,40 +7,43 @@
     function setWindDir() {
         var linkFn;
         linkFn = function (scope, element, attrs) {
+            scope.setDirection = "";
             scope.setWind = function (direction) {
 
-                function setArrowSide(side,title){
-                    angular.element('.setWindDir').children().addClass(side).attr("title",title);   
+                if ((direction > 337.5 && direction <= 360) || direction <= 22.5) {
+                    scope.setDirection = "N";
+                    return "rotated-n";
                 }
-
-                if((direction > 337.5 && direction <= 360) || direction <= 22.5){
-                    setArrowSide("rotated-n","North");
+                else if (direction > 22.5 && direction <= 67.5) {
+                    scope.setDirection = "NE";
+                    return "rotated-ne";
                 }
-                else if(direction > 22.5 && direction <= 67.5){
-                    setArrowSide("rotated-ne","North East");
+                else if (direction > 67.5 && direction <= 112.5) {
+                    scope.setDirection = "E";
+                    return "rotated-e";
                 }
-                else if(direction > 67.5 && direction <= 112.5){
-                    setArrowSide("rotated-e","East");
+                else if (direction > 112.5 && direction <= 157.5) {
+                    scope.setDirection = "SE";
+                    return "rotated-se";
                 }
-                else if(direction > 112.5 && direction <= 157.5){
-                    setArrowSide("rotated-se","South East");
+                else if (direction > 157.5 && direction <= 202.5) {
+                    scope.setDirection = "S";
+                    return "rotated-s";
                 }
-
-                else if(direction > 157.5 && direction <= 202.5){
-                    setArrowSide("rotated-s","South");
+                else if (direction > 202.5 && direction <= 247.5) {
+                    scope.setDirection = "SW";
+                    return "rotated-sw";
                 }
-                else if(direction > 202.5 && direction <= 247.5){
-                    setArrowSide("rotated-sw","South West");
+                else if (direction > 247.5 && direction <= 292.5) {
+                    scope.setDirection = "W";
+                    return "rotated-w";
                 }
-                else if(direction > 247.5 && direction <= 292.5){
-                    setArrowSide("rotated-w","West");
-                }
-
-                else if(direction > 292.5 && direction <= 337.5){
-                    setArrowSide("rotated-nw","North West");
+                else if (direction > 292.5 && direction <= 337.5) {
+                    scope.setDirection = "NW";
+                    return "rotated-nw";
                 }
             }
-            
+
         }
 
 
