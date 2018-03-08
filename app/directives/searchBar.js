4bf7be02aa7bfb310a2f5d917ca07a0ae1a3d146
Kris <kristapsrss@gmail.com>
2018-03-08 20:24:14 +0200

    Display lat/lng of selected city

---
+++ app/directives/searchBar.js
@@ -0,0 +1,54 @@
+(function () {
+    "use strict";
+
+    angular.module("WeatherApp").directive('searchBar', searchBar);
+
+
+    function searchBar() {
+        var linkFn;
+
+        linkFn = function (scope, element, attrs) {
+
+            var placeSearch, autocomplete, geocoder;
+
+            //Get autocomplete data, for cities that matches a keyword
+            scope.initAutocomplete = function () {
+                geocoder = new google.maps.Geocoder();
+                autocomplete = new google.maps.places.Autocomplete(
+                    (document.getElementById('searchTextField')), {
+                        types: ['(cities)'],
+                    });
+
+                autocomplete.addListener('place_changed', fillInAddress);
+            }
+
+
+            function fillInAddress() {
+                var place = autocomplete.getPlace();
+                codeAddress(document.getElementById('searchTextField').value);
+            }
+
+
+            //Retrive lng/lat of selected city
+            function codeAddress(address) {
+                geocoder.geocode({ 'address': address }, function (results, status) {
+                    if (status == 'OK') {
+                        alert(results[0].geometry.location);
+                    } else {
+                        alert('Geocode was not successful: ' + status);
+                    }
+                });
+            }
+
+            scope.initAutocomplete();
+        }
+
+
+
+        return {
+            restrict: 'C',
+            link: linkFn
+        }
+    }
+
+}());
